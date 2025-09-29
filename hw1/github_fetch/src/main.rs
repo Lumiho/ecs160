e9a4d9ba53f4b56172976f446f458dab2fb1b61a
Lumiho <zavalaleo715@yahoo.com>
2025-09-29 14:36:48 -0700

    initializing group ecs160 repo

---
+++ hw1/github_fetch/src/main.rs
@@ -0,0 +1,12 @@
+fn main() 
+{
+    println!("Hello, world!");
+    let c = "https://api.github.com/search/repositories?q=language:C/C++&sort=stars&order=desc&per_page=10";
+    let java = "https://api.github.com/search/repositories?q=language:Java&sort=stars&order=desc&per_page=10";
+    let rust = "https://api.github.com/search/repositories?q=language:Rust&sort=stars&order=desc&per_page=10";
+
+    println!("C: {}", c);
+    println!("Java: {}", java);
+    println!("Rust: {}", rust);
+
+}
